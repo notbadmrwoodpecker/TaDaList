9da29df5416311ceabac6135d6db3d6b301fafce
Christian <christian.arp@posteo.de>
2024-10-26 16:22:23 +0200

    feat: real database connection instead of dummy data for tasks

--- routes/web.php
+++ routes/web.php
@@ -1,51 +1,21 @@
 <?php
 
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
-
-class Task {
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    )
-    {
-
-    }
-}
-
-$tasks = [
-    new Task(
-        1,
-        'Complete Chapter 1 of PHP + Laravel Course',
-        'Do it!',
-        '',
-        false,
-        '2024-10-26 14:00:00',
-        '2024-10-26 14:00:00'
-    )
-];
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function () use($tasks) {
+Route::get('/tasks', function () {
+    $tasks = \App\Models\Task::all();
+
     return view('index', [
         'tasks' => $tasks,
     ]);
 })->name('tasks.index');
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
-    $task = collect($tasks)->firstWhere('id', $id);
-
-    if (!$task) {
-        abort(Response::HTTP_NOT_FOUND);
-    }
+Route::get('/tasks/{id}', function ($id) {
+    $task = \App\Models\Task::findOrFail($id);
 
     return view('show', [
         'task' => $task
